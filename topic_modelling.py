e5cd661ac2da456038ae7a16d1d335e2b2993d76
dang-nh <dang.nh194423@sis.hust.edu.vn>
2023-12-16 16:21:23 +0000

    add topic modelling for requirments

---
+++ topic_modelling.py
@@ -0,0 +1,55 @@
+import utils
+import numpy as np
+from sentence_transformers import SentenceTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from underthesea import word_tokenize
+from bertopic import BERTopic
+import json
+import time
+def get_topics():
+    start_time = time.time()
+    data = utils.read_jsonl("normalized_data/it_jobs.jsonl")
+    docs = utils.get_requirements(data)
+
+    vectorizer = CountVectorizer(ngram_range=(1, 2), tokenizer=word_tokenize)
+    
+    rep_models = ["keybert", {"type": "mrm", "args": {"diversity": 0.6}}, "gpt"]
+    rep_model = utils.get_representation_model(rep_models)  
+
+    embedding_model_name = 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
+    embedding_model = SentenceTransformer(embedding_model_name)
+    # embeddings = embedding_model.encode(docs, show_progress_bar=True)
+    # utils.save_embeddings(embeddings, "large_files/requirement_embeddings/embeddings.npy")
+
+    topic_model = BERTopic(
+            embedding_model=embedding_model,
+            vectorizer_model=vectorizer,
+            representation_model=rep_model,
+            verbose=True
+    )
+    
+    topics, _ = topic_model.fit_transform(docs)
+    model_save_dir = "large_files/topic_model"
+    topic_model.save(model_save_dir, serialization="safetensors", save_ctfidf=True)
+   
+    with open("topics.json", 'w') as f:
+        json.dump(topics, f)
+    print("Compute topic takes --- %s seconds ---" % (time.time() - start_time))
+
+def main():
+    data = utils.read_jsonl("normalized_data/it_jobs.jsonl")
+    reqs = utils.get_requirements(data)
+    # step 1: get embeddings
+    
+    model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+    
+    # # save embeddings
+    # with open('large_files/requirement_embeddings/embeddings.npy', 'wb') as f:
+    #     np.save(f, embeddings)
+    # step 2: get topics
+        
+    # step 3: transform documents
+    pass
+
+if __name__ == "__main__":
+    get_topics()
--- utils.py
+++ utils.py
@@ -6,7 +6,10 @@ import time
 from bertopic import BERTopic
 import os
 import json
-
+import numpy as np
+from underthesea import word_tokenize
+from bertopic.representation import KeyBERTInspired, MaximalMarginalRelevance, OpenAI
+import openai
 
 def plot_wordcloud(word_list, save_path=None):
     counter = dict(Counter(word_list))
@@ -20,13 +23,13 @@ def plot_wordcloud(word_list, save_path=None):
         plt.show()
 
 
-def get_jobs_by_positions(jobs, categories, positions):
+def get_jobs_by_positions(jobs, positions, return_id=False):
     """
     jobs: list of jobs postings
     categories: list of category for each job posting, type: string
     positions: list of  query job positions
-
     """
+    categories = get_category_str(jobs)
     positions = [f"({p})" for p in positions]
     pattern = "|".join(positions)
     jobs_id = [
@@ -35,7 +38,10 @@ def get_jobs_by_positions(jobs, categories, positions):
         if re.search(pattern, category, re.IGNORECASE)
     ]
     jobs = [jobs[i] for i in jobs_id]
-    return jobs
+    if return_id:
+        return jobs, jobs_id
+    else:
+        return jobs
 
 
 def plot_skills_wordcloud(data, save_path=None):
@@ -57,10 +63,18 @@ def plot_skills_wordcloud(data, save_path=None):
     plot_wordcloud(skills, save_path)
 
 
-def train_topic_model(docs, save_model_dir=None):
+def read_it_jobs_txt(path):
+    with open(path, "r") as f:
+        data = f.readlines()
+    data = [row.strip() for row in data]
+    data = [row.split(">") for row in data]
+    return data
+
+
+def train_topic_model(docs, save_model_dir=None, **kwargs):
     start_time = time.time()
     topic_model = BERTopic(
-        language="multilingual", calculate_probabilities=True, verbose=True
+        **kwargs, language="multilingual", calculate_probabilities=True, verbose=True
     )
     topics, probs = topic_model.fit_transform(docs)
     if save_model_dir is not None:
@@ -90,3 +104,128 @@ def write_jsonl(file_path, data):
 class BertTopic:
     def load_model(path):
         return BERTopic.load(path, serialization="safetensors")
+
+
+def read_jsonl(file_path):
+    with open(file_path, "r") as f:
+        data = [json.loads(line) for line in f]
+    return data
+
+
+def df_to_jsonl(df, file_path):
+    data = df.to_dict(orient="records")
+    write_jsonl(file_path, data)
+
+
+def load_embeddings(file_path):
+    with open(file_path, "rb") as f:
+        embeddings = np.load(f)
+    return embeddings
+
+def remove_multiple_spaces(text):
+    return re.sub(r"\s+", " ", text)
+
+def remove_punctuation(text):
+    return re.sub(r"[^\w\s]", "", text)
+
+def filter_by_length(text, min_len=4):
+    return len(text.split(" ")) >= min_len
+
+def get_requirements(data, return_post_id=False):
+    # separate by new line
+    # separate by --
+    # remove punctuation
+    # filte by length, minimum length 4
+    def process_req(req):
+        req = req.lower()
+        req = remove_punctuation(req)
+        req = remove_multiple_spaces(req)
+        req = req.strip()
+        if filter_by_length(req):
+            return req
+        else:
+            return None
+    
+    def process(req_str):
+        res = []
+        reqs = req_str.split("\n")
+        for req in reqs:
+            req = req.split("--")
+            req = [process_req(i) for i in req]
+            req = [i for i in req if i is not None]
+            res += req
+        return res
+    
+    res = []
+    req_post_id = []
+    for i, row in enumerate(data):
+        if "requirements" in row:
+            req = process(row["requirements"])
+            res += req
+            req_post_id += [i] * len(req)
+    if return_post_id:
+        return res, req_post_id
+    else:
+        return res
+
+def get_category_str(jobs):
+    categories = []
+    for job in jobs:
+        if('category' not in job):
+            categories.append("")
+        else:
+            if(isinstance(job['category'], str)):
+                categories.append(job['category'])
+            elif(isinstance(job['category'], list)):
+                categories.append(" ".join(job['category']))
+            else:
+                categories.append("")
+    return categories
+
+def get_representation_model(types):
+    res = {}
+    for i in types:
+        if isinstance(i, str):
+            model_type = i
+            args = {}
+        elif isinstance(i, dict):
+            model_type = i['type']
+            args = i['args']
+        
+        assert isinstance(args, dict)
+        
+        if(model_type == "keybert"):
+            res[model_type] = KeyBERTInspired(**args)
+        elif(model_type == "mrm"):
+            res[model_type] = MaximalMarginalRelevance(**args)
+        elif(model_type == 'gpt'):
+            client = openai.OpenAI(api_key="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")
+            prompt = """
+            I have a topic that contains the following documents:
+            [DOCUMENTS]
+            The topic is described by the following keywords: [KEYWORDS]
+
+            Based on the information above, extract a short but highly descriptive topic label of at most 5 words. Make sure it is in the following format:
+            topic: <topic label>
+            """
+            client = openai.OpenAI(api_key="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")
+            openai_model = OpenAI(client, model="gpt-3.5-turbo", exponential_backoff=True, chat=True, prompt=prompt)
+            res[model_type] = openai_model
+    return res
+
+def save_embeddings(embeddings, path):
+    with open(path, 'wb') as f:
+        np.save(f, embeddings)
+
+def save_topic_info(topic_info, path):
+    df_to_jsonl(topic_info, path)
+
+def debug_req(reqs):
+    jobs = read_jsonl("normalized_data/it_jobs.jsonl")
+    reqs = get_requirements(jobs)
+    reqs1 = [(i, len(i)) for i in reqs]
+    reqs1 = sorted(reqs1, key=lambda x : x[1])
+    reqs = [i[0] for i in reqs1]
+    reqs = "\n############\n".join(reqs)
+    with open("req.txt", 'w') as f:
+        f.write(reqs)
